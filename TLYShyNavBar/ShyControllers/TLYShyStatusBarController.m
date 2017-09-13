49b282f4c67be8ce0892d15e951c0903eeb9dcf8
Zach Radke <zach.radke@demandmedia.com>
2017-09-13 10:45:07 -0700

    Change how status bar height is calculated
    Fixes issues with iPhone X, which has a 44pt default status bar height.

--- TLYShyNavBar/ShyControllers/TLYShyStatusBarController.m
+++ TLYShyNavBar/ShyControllers/TLYShyStatusBarController.m
@@ -8,50 +8,21 @@
 
 #import "TLYShyStatusBarController.h"
 
-
-// Thanks to SO user, MattDiPasquale
-// http://stackoverflow.com/questions/12991935/how-to-programmatically-get-ios-status-bar-height/16598350#16598350
-
-static inline CGFloat AACStatusBarHeight(UIViewController *viewController)
-{
-    if ([UIApplication sharedApplication].statusBarHidden)
-    {
-        return 0.f;
-    }
-    
-    // Modal views do not overlap the status bar, so no allowance need be made for it
-    CGSize  statusBarSize = [UIApplication sharedApplication].statusBarFrame.size;
-    CGFloat statusBarHeight = MIN(statusBarSize.width, statusBarSize.height);
-    
-    UIView *view = viewController.view;
-    CGRect frame = [view.superview convertRect:view.frame toView:view.window];
-    
-    BOOL viewOverlapsStatusBar = frame.origin.y < statusBarHeight;
-    
-    if (!viewOverlapsStatusBar)
-    {
-        return 0.f;
-    }
-    
-    return statusBarHeight;
-}
-
-
 @implementation TLYShyStatusBarController
 
 - (CGFloat)_statusBarHeight
 {
-    CGFloat statusBarHeight = AACStatusBarHeight(self.viewController);
-    /* The standard status bar is 20 pixels. The navigation bar extends 20 pixels up so it is overlapped by the status bar.
-     * When there is a larger than 20 pixel status bar (e.g. a phone call is in progress or GPS is active), the center needs
-     * to shift up 20 pixels to avoid this 'dead space' being visible above the usual nav bar.
-     */
-    if (statusBarHeight > 20)
-    {
-        statusBarHeight -= 20;
-    }
-    
-    return statusBarHeight;
+	CGRect statusBarFrame = [UIApplication sharedApplication].statusBarFrame;
+	UIView *view = self.viewController.view;
+	CGRect viewFrame = [view.superview convertRect:view.frame toView:nil];
+
+	CGRect intersection = CGRectIntersection(statusBarFrame, viewFrame);
+
+	if (CGRectIsNull(intersection)) {
+		return 0.f;
+	} else {
+		return intersection.size.height;
+	}
 }
 
 - (CGFloat)maxYRelativeToView:(UIView *)superview
